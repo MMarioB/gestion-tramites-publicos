47020ea1f73847197ae0c7b690b49d2f785628c7
Mario Bravo Ovejero <mmariob196@gmail.com>
2025-10-16 14:16:20 +0200

    chore: setup inicial del proyecto con estructura de microservicios

---
+++ tramites-service/src/main/java/com/gestiontramites/tramites/TramitesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gestiontramites.tramites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramitesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TramitesServiceApplication.class, args);
+	}
+
+}
---
+++ tramites-service/src/test/java/com/gestiontramites/tramites/TramitesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestiontramites.tramites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramitesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
